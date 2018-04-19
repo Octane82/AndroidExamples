edaf5defeeb9c663c50d3cd0a293cec91b15a802
Dmitriy Karelin <everlapp@gmail.com>
2018-04-19 11:35:38 +0300

    Added RecyclerView example

---
+++ app/src/main/java/com/everlapp/androidexamples/adapters/SimpleRecyclerAdapter.java
@@ -0,0 +1,88 @@
+package com.everlapp.androidexamples.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ *
+ *
+ * https://developer.android.com/training/material/lists-cards.html
+ */
+public class SimpleRecyclerAdapter
+        extends RecyclerView.Adapter<SimpleRecyclerAdapter.SimpleViewHolder> {
+
+    /**
+     * Interface for click listener. Implement in Activity or Fragment
+     */
+    public interface OnItemClickListener {
+        void onItemClick();
+    }
+
+    private OnItemClickListener mClickListener;
+
+    public SimpleRecyclerAdapter(OnItemClickListener mClickListener) {
+        this.mClickListener = mClickListener;
+    }
+
+    @NonNull
+    @Override
+    public SimpleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SimpleViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    /**
+     * Если View разных типов
+     * @param position
+     * @return
+     */
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+
+
+    class SimpleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView mTitle;
+        private TextView mText;
+
+        public void setTitle(TextView title) {
+            this.mTitle = title;
+        }
+
+        public void setText(TextView text) {
+            this.mText = text;
+        }
+
+
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+
+            // mTitle = findViewById
+            // mText = findViewById
+        }
+
+        @Override
+        public void onClick(View v) {
+            mClickListener.onItemClick();
+        }
+    }
+
+
+}
+
