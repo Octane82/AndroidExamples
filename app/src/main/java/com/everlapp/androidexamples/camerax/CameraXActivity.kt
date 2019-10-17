4ad91337e91829ba8597922db5d05617b5df8944
macbook <d.karelin@nomera.com>
2019-10-17 10:17:58 +0300

    Complete CameraX example

--- app/src/main/java/com/everlapp/androidexamples/camerax/CameraXActivity.kt
+++ app/src/main/java/com/everlapp/androidexamples/camerax/CameraXActivity.kt
@@ -4,20 +4,27 @@ import android.Manifest
 import android.graphics.Matrix
 import androidx.lifecycle.LifecycleOwner
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Rational
 import android.util.Size
 import android.view.Surface
 import android.view.ViewGroup
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import android.widget.Toast
+import androidx.camera.core.*
 import com.everlapp.androidexamples.R
 import com.tbruyelle.rxpermissions2.RxPermissions
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_camera_x_activity.*
 import timber.log.Timber
+import java.io.File
+import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
 
+/**
+ * https://developer.android.com/training/camerax
+ */
 class CameraXActivity : AppCompatActivity(), LifecycleOwner {
 
     private val disposables = CompositeDisposable()
@@ -77,11 +84,66 @@ class CameraXActivity : AppCompatActivity(), LifecycleOwner {
         }
 
 
+        // ------ Image capture -----------------
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+                .apply {
+                    setTargetAspectRatio(Rational(1,1))
+                    // We don't set a resolution for image capture; instead, we
+                    // select a capture mode which will infer the appropriate
+                    // resolution based on aspect ration and requested mode
+                    setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        btnCapture.setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file, object: ImageCapture.OnImageSavedListener {
+
+                override fun onImageSaved(file: File) {
+                    val msg = "Photo capture succeeded: ${file.absolutePath}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+                    Timber.e("CameraXApp $msg")
+                }
+
+                override fun onError(imageCaptureError: ImageCapture.ImageCaptureError,
+                                     message: String,
+                                     ex: Throwable?) {
+                    val msg = "Photo capture failed: $message"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Timber.e("CameraXApp $msg")
+                    ex?.printStackTrace()
+                }
+            })
+        }
+
+
+        // --------------------------------------
+
+
+        // ---------- Image analysis ------------
+
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // Use a worker thread for image analysis to prevent glitches
+            val analyzerThread = HandlerThread(
+                    "LuminosityAnalysis").apply { start() }
+            setCallbackHandler(Handler(analyzerThread.looper))
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
+
+        // Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            analyzer = LuminosityAnalyzer()
+        }
+
+
         // Bind use cases to lifecycle
         // If Android Studio complains about "this" being not a LifecycleOwner
         // try rebuilding the project or updating the appcompat dependency to
         // version 1.1.0 or higher.
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, imageCapture, analyzerUseCase)
     }
 
 
@@ -106,5 +168,47 @@ class CameraXActivity : AppCompatActivity(), LifecycleOwner {
         // Finally, apply transformations to our texture view
         viewFinder.setTransform(matrix)
     }
+
+
+
+    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+
+        private var lastAnalyzedTimestamp = 0L
+
+        /**
+         * Helper extension function used to extract a byte array from an
+         * image plane buffer
+         */
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+
+        override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+            val currentTimestamp = System.currentTimeMillis()
+            // Calculate the average luma no more often than every second
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                    TimeUnit.SECONDS.toMillis(1)) {
+                // Since format in ImageAnalysis is YUV, image.planes[0]
+                // contains the Y (luminance) plane
+                val buffer = image!!.planes[0].buffer
+                // Extract image data from callback object
+                val data = buffer.toByteArray()
+                // Convert the data into an array of pixel values
+                val pixels = data.map { it.toInt() and 0xFF }
+                // Compute average luminance for the image
+                val luma = pixels.average()
+                // Log the new luma value
+                Timber.d("CameraXApp: Average luminosity: $luma")
+                // Update timestamp of last analyzed frame
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+        }
+
+    }
+
 
 }
