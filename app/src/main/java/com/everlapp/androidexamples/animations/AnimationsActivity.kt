8c76b6b5f600cb8c551cf35c82c921a56d2ef90f
D.Karelin (Noomeera office) <d.karelin@nomera.com>
2020-03-06 11:03:01 +0300

    Added animations (MotionLayout)

---
+++ app/src/main/java/com/everlapp/androidexamples/animations/AnimationsActivity.kt
@@ -0,0 +1,19 @@
+package com.everlapp.androidexamples.animations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.everlapp.androidexamples.R
+
+/**
+ * https://codelabs.developers.google.com/codelabs/motion-layout/#2
+ */
+class AnimationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_constraint_layout)
+        setContentView(R.layout.activity_step1)
+
+    }
+
+}
