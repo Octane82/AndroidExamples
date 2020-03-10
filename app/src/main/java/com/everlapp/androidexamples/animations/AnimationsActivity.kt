4b5ef097c38c5725bc6a13e6d9e51fbe6dabba0b
D.Karelin (Noomeera office) <d.karelin@nomera.com>
2020-03-10 14:46:03 +0300

    Added property animator

--- app/src/main/java/com/everlapp/androidexamples/animations/AnimationsActivity.kt
+++ app/src/main/java/com/everlapp/androidexamples/animations/AnimationsActivity.kt
@@ -1,10 +1,20 @@
 package com.everlapp.androidexamples.animations
 
+import android.animation.*
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import com.everlapp.androidexamples.R
+import kotlinx.android.synthetic.main.activity_animation_property.*
 
 /**
+ * https://codelabs.developers.google.com/codelabs/advanced-android-kotlin-training-property-animation/#0
  * https://codelabs.developers.google.com/codelabs/motion-layout/#2
  */
 class AnimationsActivity : AppCompatActivity() {
@@ -12,8 +22,125 @@ class AnimationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_constraint_layout)
-        setContentView(R.layout.activity_step1)
+        setContentView(R.layout.activity_animation_property)
 
+        btnRotate.setOnClickListener { rotater() }
+        btnTranslate.setOnClickListener { translater() }
+        btnScale.setOnClickListener { scaler() }
+        btnFading.setOnClickListener { fader() }
+        btnColorize.setOnClickListener { colorizer() }
+        btnShower.setOnClickListener { shower() }
     }
+
+
+    // ------ Animation property ---------
+
+    private fun rotater() {
+        // -360 start => 0 end (full circle)
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+        animator.disableViewDuringAnimation(btnRotate)
+        /*animator.addListener(object : AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
+                btnRotate.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                btnRotate.isEnabled = true
+            }
+        })*/
+        animator.start()
+    }
+
+    private fun translater() {
+        // 200f - pixels
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(btnTranslate)
+        animator.start()
+    }
+
+    private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(btnScale)
+        animator.start()
+    }
+
+    private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(btnFading)
+        animator.start()
+    }
+
+    private fun colorizer() {
+        //val animator = ObjectAnimator.ofInt(star.parent, "backgroundColor", Color.BLACK, Color.RED).start()
+
+        // more smooth animation
+        var animator = ObjectAnimator.ofArgb(star.parent, "backgroundColor", Color.BLACK, Color.RED)
+        animator.duration = 500
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(btnColorize)
+        animator.start()
+    }
+
+    private fun shower() {
+        val container = star.parent as ViewGroup
+        val containerW = container.width
+        val containerH = container.height
+        var starW: Float = star.width.toFloat()
+        var starH: Float = star.height.toFloat()
+        // Create new star
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star_outline)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT)
+        container.addView(newStar)
+
+        newStar.scaleX = Math.random().toFloat() * 1.5f + .1f
+        newStar.scaleY = newStar.scaleX
+        starW *= newStar.scaleX
+        starH *= newStar.scaleY
+        // Position the new star (initial star information)
+        newStar.translationX = Math.random().toFloat() * containerW - starW / 2
+
+        val mover = ObjectAnimator.ofFloat(newStar, View.TRANSLATION_Y, -starH, containerH + starH)
+        mover.interpolator = AccelerateInterpolator(1f)
+        val rotator = ObjectAnimator.ofFloat(newStar, View.ROTATION, (Math.random() * 1080).toFloat())
+        rotator.interpolator = LinearInterpolator()
+        // Run the animations in parallel
+        val set = AnimatorSet()
+        set.playTogether(mover, rotator)
+        set.duration = (Math.random() * 1500 + 500).toLong()
+        set.addListener(object : AnimatorListenerAdapter(){
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                container.removeView(newStar)
+            }
+        })
+        set.start()
+    }
+
+
+    // extension helper method
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                view.isEnabled = true
+            }
+        })
+    }
+
 
 }
